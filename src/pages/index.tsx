320b32407c2cfc8f57c1c8b6dbf6b878a0a15912
Devang1224 <97470914+Devang1224@users.noreply.github.com>
2023-11-12 00:34:48 +0530

    added favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,16 +4,22 @@ import NavBar from '@/components/Navbar/NavBar'
 import Footer from '@/components/Footer/Footer'
 import Body from '@/components/Body/Body'
 import Subscribe from '@/components/SubscribeWindow/Subscribe'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
+    <>
+    <Head>
+        <title>KoinX</title>
+    </Head>
    <div>
      <NavBar/>
      <Body/>
      <Subscribe/>
      <Footer/>
    </div>
+   </>
   )
 }
